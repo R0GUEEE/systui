754eac5bbbe9871c9bca8d464d1f93479dbe42d5
R0GUEEE <41440185+R0GUEEE@users.noreply.github.com>
2026-09-19 20:12:01 -0400

    Fix ShellCheck directive formatting

--- src/features/80-sysconfig-init-utility-installer.sh
+++ src/features/80-sysconfig-init-utility-installer.sh
@@ -99,7 +99,8 @@ sysconfig_init_install_packages() { # <provider> <package-manager>
 sysconfig_init_packages_to_array() { # <provider> <pm> <array-name>
     local provider="$1" pm="$2" array_name="$3" raw
     raw=$(sysconfig_init_install_packages "$provider" "$pm") || return 1
-    # shellcheck disable=SC2229 -- array_name intentionally names the destination array.\n    read -r -a "$array_name" <<< "$raw"
+    # shellcheck disable=SC2229 -- array_name intentionally names the destination array.
+    read -r -a "$array_name" <<< "$raw"
 }
 
 sysconfig_init_pm_install() { # <pm> <packages...>
